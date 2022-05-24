29bbd698b92280660a2fb620d71b35191598b3b2
aciernoantonio <aciernoantonio.int@gmail.com>
2022-05-24 15:00:52 +0200

    add header section

--- layout/head.php
+++ layout/head.php
@@ -7,7 +7,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="./assets/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
   </head>
   <body>
+
+  <header>
+
+  <header>
+
+    <div class="container">
+
+        <div class="row align-items-center justify-content-between">
+
+        <div class="col">
+            <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="">
+        </div>
+
+    </div>
+
+
+</header>
+
+  </header>
