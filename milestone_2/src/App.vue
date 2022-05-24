53011188e13ecf1e1e8bc7c67c8920d826bfb2ed
aciernoantonio <aciernoantonio.int@gmail.com>
2022-05-24 16:12:55 +0200

    mileston_2 Initial commit

---
+++ milestone_2/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+
+    <SiteHeader/>
+    <SiteMain/>
+
+  </div>
+</template>
+
+<style lang="scss">
+
+@import "@/assets/scss/style.scss";
+
+</style>
+
+<script>
+import SiteHeader from "@/components/HeaderComponent.vue"
+import SiteMain from "@/components/MainComponent.vue"
+
+
+export default {
+  name: 'App',
+  components: {
+    SiteHeader,
+    SiteMain
+  }
+}
+</script>
+
+
